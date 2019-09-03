bcf3275c85729bcbe72a09aef7527796967230cf
liquidmavis <869584715@qq.com>
2019-09-03 09:55:04 +0800

    test

---
+++ gsmall-user/src/main/java/com/liquid/gsmalluser/GsmallUserApplication.java
@@ -0,0 +1,13 @@
+package com.liquid.gsmalluser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsmallUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsmallUserApplication.class, args);
+	}
+
+}
---
+++ gsmall-user/src/test/java/com/liquid/gsmalluser/GsmallUserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liquid.gsmalluser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GsmallUserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
